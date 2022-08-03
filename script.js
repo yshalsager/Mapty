0e3d69b43a034663222fb8c5b7d14cd24df6060d
yshalsager <ysh-alsager@hotmail.com>
2022-08-03 10:48:19 +0200

    006 Add leaflet

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  /**
+  `Geolocation.getCurrentPosition()`: Retrieves the device's current location.
+  - It takes two parameters, a callback function to run on success and another one on failure.
+  - The success callback function takes a GeolocationPosition object as its sole input parameter.
+  **/
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // Use destructuring to get the latitude and longitude from the position.coords object.
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+      // 'map' is the id of the div where the map will be displayed
+      // 'coords' is the coordinates of the map
+      // 'SetView' takes two parameters: the coordinates and the zoom level of the map
+      const map = L.map('map').setView(coords, 13);
+      // 'L' is the Leaflet library entry point like namespace
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+      }).addTo(map);
+    },
+    function () { alert('Could not get your position') });
+}
