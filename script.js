4a297262f8496448e9c0802583e47416c31057f8
yshalsager <ysh-alsager@hotmail.com>
2022-08-03 12:21:21 +0200

    008 Initial form handling

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent; // Make map and mapEvent objects accessible to form event listener
+
 if (navigator.geolocation) {
   /**
   `Geolocation.getCurrentPosition()`: Retrieves the device's current location.
@@ -27,26 +29,42 @@ if (navigator.geolocation) {
       // 'map' is the id of the div where the map will be displayed
       // 'coords' is the coordinates of the map
       // 'SetView' takes two parameters: the coordinates and the zoom level of the map
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
       // 'L' is the Leaflet library entry point like namespace
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '© OpenStreetMap'
       }).addTo(map);
       // Use 'on' method to add a click listener to the map
-      map.on('click', function (mapEvent) {
-        // console.log(mapEvent);
-        // Use destructuring to get the latitude and longitude from the mapEvent.latlng object.
-        const { lat, lng } = mapEvent.latlng;
-        // Use the Leaflet library to add a marker to the map
-        const marker = L.marker([lat, lng]).addTo(map).bindPopup(L.popup({
-          maxWidth: 250,
-          minHeight: 100,
-          autoClose: false,
-          closeOnClick: false,
-          className: 'running-popup'
-        })).setPopupContent("Workout").openPopup();
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden'); // Show the form
+        inputDistance.focus(); // Focus on the distance input
       });
     },
     function () { alert('Could not get your position') });
 }
+
+// Add an event listener to the form
+form.addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevent the form from submitting
+  // Clear form fields
+  [inputDistance, inputDuration, inputCadence, inputElevation].forEach(input => input.value = '');
+  // console.log(mapEvent);
+  // Use destructuring to get the latitude and longitude from the mapEvent.latlng object.
+  const { lat, lng } = mapEvent.latlng;
+  // Use the Leaflet library to add a marker to the map
+  const marker = L.marker([lat, lng]).addTo(map).bindPopup(L.popup({
+    maxWidth: 250,
+    minHeight: 100,
+    autoClose: false,
+    closeOnClick: false,
+    className: 'running-popup'
+  })).setPopupContent("Workout").openPopup();
+});
+
+// Add an event listener to the form type change
+inputType.addEventListener('change', () => {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
